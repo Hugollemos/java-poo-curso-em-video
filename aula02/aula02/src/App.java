c17916619df7cb09df5ab5665cfb6d74f4ebdfcf
Hugo Lemos Da Silva <hugolemosdasilva.hu@gmail.com>
2021-06-19 12:04:17 -0300

    first commit

--- aula02/aula02/src/App.java
+++ aula02/aula02/src/App.java
@@ -4,11 +4,12 @@ public class App {
         c1.cor = "Azul";
         c1.ponta = 0.5f;
         c1.tampar();
-        c1.rabiscar();
+        c1.rabiscar(); 
+        c1.status(); 
 
         Caneta c2 = new Caneta();
-        c2.modelo = "HL";
-        c2.cor = "preta";
+        c2.modelo = "Preta";
+        c2.ponta = 10.0f;
         c2.destampar();
         c2.rabiscar();
     }
